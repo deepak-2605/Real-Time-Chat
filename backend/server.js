71856ef98201b7683e6b4a762f8eb922409f727c
adityabhushan-13 <adityabhushan823@gmail.com>
2023-06-16 11:59:49 +0530

    schema added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connecttoDB = require("./config/db");
 
 const app = express();
 dotenv.config();
+connecttoDB();
 
 app.get("/", (req, res) => {
     res.send("API is running");
